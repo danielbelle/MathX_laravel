b3628ca38b5ffc9b0f0a8715d18d3e5394b2887b
danielbelle <henrique.danielb@gmail.com>
2025-02-19 12:41:07 -0300

    010

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -67,7 +67,26 @@ class MainController extends Controller
 
     public function printExercises()
     {
-        echo "Print Exercises";
+        // check if exercises are in session
+        if (!session()->has('exercises')) {
+            return redirect()->route('home');
+        }
+
+        $exercises = session('exercises');
+
+        echo '<pre>';
+        echo '<h1>Exercícios de Matemática </h1> (' . env('APP_NAME') . ')</h1>';
+
+        foreach ($exercises as $exercise) {
+            echo '<h2><small>' . str_pad($exercise['exercise_number'], 2, "0", STR_PAD_LEFT) . ' » </small>' . $exercise['exercise'] . '</h2>';
+        }
+
+        // solution
+        echo '<hr>';
+        echo '<small> Soluções </small><br>';
+        foreach ($exercises as $exercise) {
+            echo '<small>' . str_pad($exercise['exercise_number'], 2, "0", STR_PAD_LEFT) . ' » ' . $exercise['solution'] . '</small><br>';
+        }
     }
 
     public function exportExercises()
